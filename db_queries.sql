b36ca59316fbbe97a0bde1a020cc4efbb0e9e9d2
Reddgr <asocall@gmail.com>
2025-04-05 22:17:59 +0200

    CLI and query examples from my project

--- db_queries.sql
+++ db_queries.sql
@@ -30,3 +30,10 @@ WHERE date BETWEEN '2025-01-06' AND '2025-01-18';
 
 COPY (SELECT date, IBM FROM close_prices
 WHERE date BETWEEN '2025-02-01' AND '2025-02-10') TO 'ibm_close_prices_query.txt';
+
+
+SELECT date, ticker, security, CAST(marketCap AS DOUBLE) as marketCap, currency
+FROM maestro
+WHERE currency IN ('USD', 'EUR')
+ORDER BY marketCap DESC
+LIMIT 20;
